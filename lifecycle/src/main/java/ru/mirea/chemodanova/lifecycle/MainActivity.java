a34708d51ba7bc610613c8a30cc91e5095f2ea3e
aliceech <chemodanova70@icloud.com>
2022-05-10 21:22:42 +0300

    Initial commit

---
+++ lifecycle/src/main/java/ru/mirea/chemodanova/lifecycle/MainActivity.java
@@ -0,0 +1,19 @@
+package ru.mirea.chemodanova.lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.OnLifecycleEvent;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Server server;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        server = new Server();
+        getLifecycle().addObserver(server);
+    }
+}
